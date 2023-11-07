f3f27f14871f79243fe3f0252ee2405191eeae41
Mo DeJong <mo@modejong.com>
2023-11-06 18:50:50 -0800

    Initial Commit

---
+++ ExampleWebview/ExampleWebviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleWebviewApp.swift
+//  ExampleWebview
+//
+//  Created by Mo DeJong on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct ExampleWebviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
